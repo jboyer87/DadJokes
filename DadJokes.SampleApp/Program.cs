ea3e5780e007e6fe81f4696a7eae76b37fa06dee
James J. Boyer <james.boyer87@gmail.com>
2020-03-05 13:56:34 -0800

    Fixed formatting, add a few methods to sample app to clean up display/prompt code

--- DadJokes.SampleApp/Program.cs
+++ DadJokes.SampleApp/Program.cs
@@ -28,46 +28,85 @@ namespace DadJokes.SampleApp
 
 			DisplayJoke(randomJoke);
 
-			List<JokeResult> dogJokes = Wrapper.GetJokesContaining(connection, "dog");
+			string term = PromptForSearchTerm();
 
-			var groupedDogJokes = from joke in dogJokes
-								  group joke by joke.Length into newGroup
-								  orderby newGroup.Key
-								  select newGroup;
+			// Grab matching jokes from the API
+			List<JokeResult> matchingJokes = Wrapper.GetJokesContaining(connection, term);
 
-			foreach(var group in groupedDogJokes)
+			if(matchingJokes.Count > 0)
 			{
-				Console.WriteLine($"Found {group.Count()} {group.Key} jokes:");
-				foreach(var joke in group)
+				Console.WriteLine(matchingJokes.Count + " jokes were found:");
+
+				var jokesGroupedByLength = from joke in matchingJokes
+									  group joke by joke.Length into newGroup
+									  orderby newGroup.Key
+									  select newGroup;
+
+				// Display the jokes grouped by length.
+				foreach (var group in jokesGroupedByLength)
 				{
-					Console.WriteLine("\n" + joke.Joke + "\n");
+					Console.WriteLine($"{group.Count()} {group.Key} jokes:");
+
+					foreach (var joke in group)
+					{
+						Console.WriteLine("\n" + joke.Joke + "\n");
+					}
 				}
+			}
+			else
+			{
+				Console.WriteLine("No jokes were found matching your search term.");
 			}
 		}
 
 		#region [Private Methods]
 
+		/// <summary>
+		/// Displays the <paramref name="joke"/> in the console along with some information about 
+		/// its length, word count, and ID.
+		/// </summary>
+		/// <param name="joke">The joke to display.</param>
 		private static void DisplayJoke(JokeResult joke)
 		{
 			var jokeSize = joke.Length;
 
 			if (jokeSize == JokeLength.Small)
 			{
-				Console.WriteLine("This joke is short and sweet...");
+				Console.WriteLine("(This joke is short and sweet...)");
 			}
 			else if (jokeSize == JokeLength.Medium)
 			{
-				Console.WriteLine("This joke is medium sized, or as Goldilocks would say: " +
-					"\"Just Right\"!");
+				Console.WriteLine("(This joke is medium sized, or as Goldilocks would say: " +
+					"\"Just Right\"!)");
 			}
 			else
 			{
-				Console.WriteLine("This joke is a long one - a real doozy!");
+				Console.WriteLine("(This joke is a long one - a real doozy!)");
 			}
 
-			Console.WriteLine("Joke:" + joke.Joke);
+			Console.WriteLine("Joke: " + joke.Joke);
 			Console.WriteLine("Joke ID: " + joke.Id);
 			Console.WriteLine("Joke word count: " + joke.WordCount);
+		}
+
+		/// <summary>
+		/// Prompts the user for a search term and then returns it.
+		/// </summary>
+		/// <returns>A search term from the user.</returns>
+		private static string PromptForSearchTerm()
+		{
+			string term = String.Empty;
+
+			while(String.IsNullOrWhiteSpace(term))
+			{
+				Console.Write("\nEnter a term to find some jokes: ");
+
+				term = Console.ReadLine();
+			}
+
+			Console.WriteLine($"You entered \"{term}\". Let's find some jokes.");
+
+			return term;
 		}
 
 		#endregion
