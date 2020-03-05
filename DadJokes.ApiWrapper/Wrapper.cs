0ce40d49e956700849d5b74c58a471349e3e8f74
James J. Boyer <james.boyer87@gmail.com>
2020-03-05 09:38:27 -0800

    Added ApiWrapper project

---
+++ DadJokes.ApiWrapper/Wrapper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace DadJokes.ApiWrapper
+{
+	/// <summary>
+	/// The <c>Wrapper</c> class contains methods that wrap several common API endpoints into
+	/// simpler method calls.
+	/// </summary>
+	public class Wrapper
+	{
+		#region [Public Methods]
+
+
+		#endregion
+	}
+}
